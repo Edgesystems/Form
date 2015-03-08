6f60b3eb65fcd9364b3c5e45521aab5b46481271
root <niclas@edgesystems.se>
2015-03-08 11:21:00 +0100

    file upload

--- src/element/File.php
+++ src/element/File.php
@@ -2,5 +2,16 @@
 
 namespace edge\form\element;
 
-class File {
+class File extends Element {
+	protected $attr = array(
+		'type' => 'file'
+	);
+	
+	public function __construct( $name, array $attr = array() ) {
+		parent::__construct( $name, $attr );
+	}
+	
+	public function render() {
+		return '<input' . $this->renderAttr() . ' />';
+	}
 }
