90b32b09755d0d3a71c62ba6adce3939d094c1c7
Niclas Larsson <niclas@edgesystems.se>
2015-05-13 10:13:21 +0200

    number

--- src/element/Number.php
+++ src/element/Number.php
@@ -2,5 +2,18 @@
 
 namespace edge\form\element;
 
-class Number {
+class Number extends Element {
+	protected $attr = array(
+		'type' => 'number'
+	);
+
+	public function __construct( $name, array $attr = array() ) {
+		parent::__construct( $name, $attr );
+
+		// $this->addRule( static::EMAIL, _(':value is not a valid email.') );
+	}
+
+	public function render() {
+		return '<input' . $this->renderAttr() . ' />';
+	}
 }
