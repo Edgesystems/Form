974bd585dd41ae72787a1059f618599d9840d395
root <niclas@edgesystems.se>
2015-08-13 16:57:44 +0200

    Make File be submitted

--- src/element/File.php
+++ src/element/File.php
@@ -6,12 +6,16 @@ class File extends Element {
 	protected $attr = array(
 		'type' => 'file'
 	);
-	
+
 	public function __construct( $name, array $attr = array() ) {
 		parent::__construct( $name, $attr );
 	}
-	
+
 	public function render() {
 		return '<input' . $this->renderAttr() . ' />';
+	}
+
+	public function isSubmitted() {
+		return isset( $_FILES[$this->attr['name']] );
 	}
 }
